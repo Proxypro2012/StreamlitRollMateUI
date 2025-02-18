264c8ffe18c33107e98fd8c6dda25a4433654a5c
Proxypro2012 <kabir.tiwari@outlook.com>
2025-02-18 17:22:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,18 +17,31 @@ else:
     network_names = list(set(entry['networkName'] for entry in data))
     class_names = list(set(entry['classname'] for entry in data))
     dates = list(set(entry['date'] for entry in data))
+    usersid = list(set(entry['userid'] for entry in data))
+    checkin_time = list(set(entry['checkinTime'] for entry in data))
+    first_name = list(set(entry['firstName'] for entry in data))
+    last_name = list(set(entry['lastName'] for entry in data))
 
     # Sidebar filters
     st.sidebar.header("Filters")
-    selected_network = st.sidebar.selectbox("Select Network Name", ["All"] + network_names)
-    selected_class = st.sidebar.selectbox("Select Class Name", ["All"] + class_names)
-    selected_date = st.sidebar.selectbox("Select Date", ["All"] + dates)
+    selected_network = st.sidebar.selectbox("Select Network Name", ["All"] + network_names, key="network_name")
+    selected_class = st.sidebar.selectbox("Select Class Name", ["All"] + class_names, key="class_name")
+    selected_date = st.sidebar.selectbox("Select Date", ["All"] + dates, key="date")
+    selected_userid = st.sidebar.selectbox("Select User ID", ["All"] + usersid, key="userid")
+    selected_checkin_time = st.sidebar.selectbox("Select Checkin Time", ["All"] + checkin_time, key="checkin_time")
+    selected_first_name = st.sidebar.selectbox("Select First Name", ["All"] + first_name, key="first_name")
+    selected_last_name = st.sidebar.selectbox("Select Last Name", ["All"] + last_name, key="last_name")
 
     # Filter data based on selections
     filtered_data = [entry for entry in data if 
                      (selected_network == "All" or entry['networkName'] == selected_network) and
                      (selected_class == "All" or entry['classname'] == selected_class) and
-                     (selected_date == "All" or entry['date'] == selected_date)]
+                     (selected_date == "All" or entry['date'] == selected_date) and
+                     (selected_userid == "All" or entry['userid'] == selected_userid) and
+                     (selected_checkin_time == "All" or entry['checkinTime'] == selected_checkin_time) and
+                     (selected_first_name == "All" or entry['firstName'] == selected_first_name) and
+                     (selected_last_name == "All" or entry['lastName'] == selected_last_name)
+    ]
 
     if not filtered_data:
         st.write("No matching records found.")
